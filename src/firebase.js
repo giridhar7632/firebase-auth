3807bb59326741aec87fefa551229d84a5f8ea04
giridhar7632 <giridhar.talla2002@gmail.com>
2021-01-04 11:12:44 +0530

    error alert

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,9 @@ import firebase from "firebase/app"
 import "firebase/auth"
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyBVSC0B0fb83U_OIdhMYsajg4CuZwlnUuo",
-  authDomain: "authentication-ce8b8.firebaseapp.com",
-  projectId: "authentication-ce8b8",
-  storageBucket: "authentication-ce8b8.appspot.com",
-  messagingSenderId: "232567302036",
-  appId: "1:232567302036:web:932fe25b806bd358dcd181"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID
 })
 
 export const auth = app.auth()
